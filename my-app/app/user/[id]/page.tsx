e30d3386970a9371b58bd92a0f846969818b19f9
Zinnk <viniciusnxar@gmail.com>
2026-01-12 14:42:56 -0300

    user id

---
+++ my-app/app/user/[id]/page.tsx
@@ -0,0 +1,8 @@
+const User = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+
+  // pode da fetch usando o id como parametro / ou seja na URL /user/IDdoUser
+  return <>userprofile: {id}</>;
+};
+
+export default User;
