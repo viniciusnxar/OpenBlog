c4646b862c28b3bbf3a866a034798ef23c6bd395
Zinnk <viniciusnxar@gmail.com>
2026-01-12 14:29:52 -0300

    Creating the routes
    Just remember, inside layout.tsx the {children} are all the routes that can be shown, like /login, and the navbar on top is included for all the pages as the same

---
+++ my-app/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+    return ( <h3>ola</h3> );
+}
+ 
+export default Login;
---
+++ my-app/app/(auth)/register/page.tsx
@@ -0,0 +1,5 @@
+const Register = () => {
+    return ( <h3>register</h3> );
+}
+ 
+export default Register;
